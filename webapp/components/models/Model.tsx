fd9f70714fb6e5281e02e1c4140f063bb8bdae36
mikbry <mik@miklabs.com>
2024-01-04 19:31:42 +0100

    feat: add shadcn/ui table and scrollarea

--- webapp/components/models/Model.tsx
+++ webapp/components/models/Model.tsx
@@ -27,11 +27,14 @@
 
 'use client';
 
+import { DownloadIcon } from '@radix-ui/react-icons';
 import useBackend from '@/hooks/useBackend';
 import useTranslation from '@/hooks/useTranslation';
 import { Model } from '@/types';
 import { getDownloads, getEntityName, getResourceUrl } from '@/utils/data/models';
 import Parameter from '../common/Parameter';
+import { Button } from '../ui/button';
+import { Table, TableBody, TableRow, TableCell, TableHeader, TableHead } from '../ui/table';
 
 function ModelView({ modelId, collection }: { modelId?: string; collection: Model[] }) {
   const { backendContext } = useBackend();
@@ -57,7 +60,7 @@ function ModelView({ modelId, collection }: { modelId?: string; collection: Mode
       <div className="transition-width relative flex h-full w-full flex-1 flex-col items-stretch overflow-hidden">
         <div className="flex-1 overflow-hidden">
           <div className="flex flex-col items-center text-xs">
-            <div className="justify-left flex w-full flex-row items-center gap-1 bg-neutral-50 p-3 text-neutral-500 dark:bg-neutral-900 dark:text-neutral-300">
+            <div className="justify-left flex w-full w-full flex-row items-center justify-between gap-1 bg-neutral-50 p-3 text-neutral-500 dark:bg-neutral-900 dark:text-neutral-300">
               <div className="mx-3 flex h-7 flex-row items-center px-2">
                 <span className="gap-1 py-1 capitalize text-neutral-700 dark:text-neutral-500">
                   {`${model.creator || getEntityName(model.author)}`}
@@ -66,6 +69,11 @@ function ModelView({ modelId, collection }: { modelId?: string; collection: Mode
                 <span className="items-center truncate truncate px-2 dark:text-neutral-300">
                   {model.name}
                 </span>
+              </div>
+              <div>
+                <Button variant="secondary" className="mr-4">
+                  Download
+                </Button>
               </div>
             </div>
           </div>
@@ -120,27 +128,40 @@ function ModelView({ modelId, collection }: { modelId?: string; collection: Mode
                   />
                 </div>
                 {downloads.length > 0 && (
-                  <div className="flex w-full flex-col gap-2 text-sm">
+                  <div className="flex w-full flex-col text-sm">
                     <p className="py-4">{t('Downloadable implementations')}</p>
-                    <table className="w-full">
-                      <tbody className="flex h-[30vh] flex-col justify-between overflow-y-auto">
+                    <Table className="w-full">
+                      <TableHeader>
+                        <TableRow>
+                          <TableHead>File</TableHead>
+                          <TableHead>Size</TableHead>
+                          <TableHead>Description</TableHead>
+                          <TableHead>Action</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
                         {downloads.map((download) => (
-                          <tr
+                          <TableRow
                             onClick={() => {}}
                             key={download.id || download.name}
-                            className="mb-2 flex w-full cursor-pointer hover:bg-neutral-100 dark:hover:bg-neutral-800"
+                            className="hover:bg-neutral-100 dark:hover:bg-neutral-800"
                           >
-                            <td className="w-1/3 truncate p-1">{download.name}</td>
-                            <td className="w-2/3 p-1">
-                              <p className="w-full truncate">
-                                <span>{`${(download.size || 0).toFixed(1)}Gb`}</span>
-                                <span className="ml-4">{download.recommendations || ''}</span>
-                              </p>
-                            </td>
-                          </tr>
+                            <TableCell className="truncate">{download.name}</TableCell>
+                            <TableCell className="truncate">
+                              <span>{`${(download.size || 0).toFixed(1)}Gb`}</span>
+                            </TableCell>
+                            <TableCell className="truncate">
+                              <span>{download.recommendations || ''}</span>
+                            </TableCell>
+                            <TableCell aria-label="download">
+                              <Button variant="ghost" size="icon">
+                                <DownloadIcon />
+                              </Button>
+                            </TableCell>
+                          </TableRow>
                         ))}
-                      </tbody>
-                    </table>
+                      </TableBody>
+                    </Table>
                   </div>
                 )}
               </div>
