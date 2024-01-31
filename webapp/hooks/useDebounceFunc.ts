61cb04d0e98d009828b001c4103b185711106a20
mikbry <mik@miklabs.com>
2024-01-31 17:40:02 +0100

    chore: prettier

--- webapp/hooks/useDebounceFunc.ts
+++ webapp/hooks/useDebounceFunc.ts
@@ -1,11 +1,11 @@
 // Copyright 2024 mik
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,14 +15,13 @@
 import { useEffect } from 'react';
 
 export default function useDebounceFunc<T>(callback: (v: T) => void, value: T, delay: number) {
-
-    useEffect(() => {
-        const handler = setTimeout(() => {
-            if (!value) return;
-            callback(value);
-        }, delay);
-        return () => {
-            clearTimeout(handler);
-        };
-    }, [callback, value, delay]);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if (!value) return;
+      callback(value);
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [callback, value, delay]);
 }
