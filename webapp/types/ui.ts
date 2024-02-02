11d09984e2bf3d597392502205e907606323d3a8
mikbry <mik@miklabs.com>
2024-02-02 10:42:16 +0100

    chore: prettier

--- webapp/types/ui.ts
+++ webapp/types/ui.ts
@@ -1,11 +1,11 @@
 // Copyright 2024 mik
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,41 +16,41 @@ import { LucideIcon } from 'lucide-react';
 
 // Ui types
 export type Item = {
-    name: string;
-    href?: string;
-    page?: string;
-    icon?: LucideIcon;
-    items?: Array<Item>;
-    flex?: number;
-    hidden?: boolean;
-    modal?: boolean;
-    shortcut?: string;
+  name: string;
+  href?: string;
+  page?: string;
+  icon?: LucideIcon;
+  items?: Array<Item>;
+  flex?: number;
+  hidden?: boolean;
+  modal?: boolean;
+  shortcut?: string;
 };
 
 export type MenuItem = {
-    variant?: 'link' | 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost';
-    label: string;
-    value?: string;
-    group?: string;
-    icon?: unknown;
-    selected?: boolean;
-    onSelect?: (data: string) => void;
+  variant?: 'link' | 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost';
+  label: string;
+  value?: string;
+  group?: string;
+  icon?: unknown;
+  selected?: boolean;
+  onSelect?: (data: string) => void;
 };
 
 export enum MenuAction {
-    ArchiveConversation = 'archive-conversation',
-    UnarchiveConversation = 'unarchive-conversation',
-    DeleteConversation = 'delete-conversation',
-    ChangeView = 'change-view',
+  ArchiveConversation = 'archive-conversation',
+  UnarchiveConversation = 'unarchive-conversation',
+  DeleteConversation = 'delete-conversation',
+  ChangeView = 'change-view',
 }
 
 export enum ViewName {
-    Recent = 'recent',
-    Archives = 'archives',
-};
+  Recent = 'recent',
+  Archives = 'archives',
+}
 
 export enum Page {
-    Threads = '/threads',
-    Models = '/models',
-    Providers = '/providers',
+  Threads = '/threads',
+  Models = '/models',
+  Providers = '/providers',
 }
