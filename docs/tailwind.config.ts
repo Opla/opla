5127fc1a4df2dc184e30d21cea52bd5425af5eab
mikbry <mik@miklabs.com>
2024-03-08 15:42:59 +0100

    chore: another fix for tailwindcss-animate build-docs in ga

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -1,6 +1,97 @@
 import type { Config } from 'tailwindcss';
 
-import appconfig from '../webapp/tailwind.config';
+// import appconfig from '../webapp/tailwind.config';
+
+// Dirty fix for tailwindcss-animate
+const appconfig: Config = {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './app/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    darkMode: 'class',
+    prefix: '',
+    theme: {
+      container: {
+        center: true,
+        padding: '2rem',
+        screens: {
+          '2xl': '1400px',
+        },
+      },
+      extend: {
+        fontSize: {
+          '2xs': ['0.625rem', { lineHeight: '1rem' }],
+          '3xs': ['0.5rem', { lineHeight: '0.75rem' }],
+        },
+        colors: {
+          border: 'hsl(var(--border))',
+          input: 'hsl(var(--input))',
+          ring: 'hsl(var(--ring))',
+          background: 'hsl(var(--background))',
+          foreground: 'hsl(var(--foreground))',
+          primary: {
+            DEFAULT: 'hsl(var(--primary))',
+            foreground: 'hsl(var(--primary-foreground))',
+          },
+          secondary: {
+            DEFAULT: 'hsl(var(--secondary))',
+            foreground: 'hsl(var(--secondary-foreground))',
+          },
+          destructive: {
+            DEFAULT: 'hsl(var(--destructive))',
+            foreground: 'hsl(var(--destructive-foreground))',
+          },
+          muted: {
+            DEFAULT: 'hsl(var(--muted))',
+            foreground: 'hsl(var(--muted-foreground))',
+          },
+          accent: {
+            DEFAULT: 'hsl(var(--accent))',
+            foreground: 'hsl(var(--accent-foreground))',
+          },
+          popover: {
+            DEFAULT: 'hsl(var(--popover))',
+            foreground: 'hsl(var(--popover-foreground))',
+          },
+          card: {
+            DEFAULT: 'hsl(var(--card))',
+            foreground: 'hsl(var(--card-foreground))',
+          },
+        },
+        borderRadius: {
+          lg: 'var(--radius)',
+          md: 'calc(var(--radius) - 2px)',
+          sm: 'calc(var(--radius) - 4px)',
+        },
+        keyframes: {
+          'accordion-down': {
+            from: { height: '0' },
+            to: { height: 'var(--radix-accordion-content-height)' },
+          },
+          'accordion-up': {
+            from: { height: 'var(--radix-accordion-content-height)' },
+            to: { height: '0' },
+          },
+        },
+        animation: {
+          'accordion-down': 'accordion-down 0.2s ease-out',
+          'accordion-up': 'accordion-up 0.2s ease-out',
+          bounce200: 'bounce 1s infinite 200ms',
+          bounce400: 'bounce 1s infinite 400ms',
+        },
+      },
+    },
+    safelist: [
+      {
+        pattern: /(bg|text|border)-(yellow|red|green|gray|blue|orange)-(500|400)/,
+      },
+      'line-through',
+    ],
+    // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+    // plugins: [require('tailwindcss-animate')],
+  };
 
 const config = {
     ...appconfig, darkMode: ['class', '[data-theme="dark"]'], corePlugins: {
