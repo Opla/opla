b9f6bdb09976566c9663cb8685f51dc50cf62f83
mikbry <mik@miklabs.com>
2024-03-27 12:48:20 +0100

    fix: server start use available model if no model #609

--- webapp/native/src/llm/llama_cpp.rs
+++ webapp/native/src/llm/llama_cpp.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::{ error::Error, llm::{ LlmQueryCompletion, LlmResponseError }, store::ServerParameters };
+use crate::{ llm::{ LlmQueryCompletion, LlmResponseError }, store::ServerParameters };
 
 use tauri::Runtime;
 use eventsource_stream::Eventsource;
