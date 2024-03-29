f2ea0b338f097fb0773cd47add8bf0fd3b734cd5
mikbry <mik@miklabs.com>
2024-03-29 19:44:28 +0100

    feat: display app version in settings #662

--- webapp/modals/settings/Settings.tsx
+++ webapp/modals/settings/Settings.tsx
@@ -15,6 +15,7 @@
 'use client';
 
 import NavContainer from '@/components/common/NavContainer';
+import { Separator } from '@/components/ui/separator';
 import useTranslation from '@/hooks/useTranslation';
 import { cn } from '@/lib/utils';
 
@@ -45,41 +46,47 @@ export default function Settings({
   const { t } = useTranslation();
 
   return (
-    <div className="flex h-full w-full">
-      <div className="navSettings">
-        <NavContainer>
-          <div className="text-l text-ellipsis break-all p-3 font-semibold">{t('Settings')}</div>
-          <ul className="flex flex-1 flex-col gap-1 p-1">
-            {menu.map((item) => (
-              <li key={item.id}>
-                <div
-                  className={cn(
-                    item.href === tab || item.hrefAlias === tab
-                      ? 'text-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring'
-                      : 'text-muted-foreground',
-                    'flex cursor-pointer flex-row items-center break-all rounded-md px-2 py-2 transition-colors duration-200 hover:text-accent-foreground',
-                  )}
-                  role="button"
-                  tabIndex={0}
-                  onKeyUp={(e) => {
-                    e.stopPropagation();
-                  }}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    onTabChanged(item.href as string);
-                  }}
-                >
-                  <div className="relative flex-1 overflow-hidden text-ellipsis break-all">
-                    {t(item.name)}
+    <div className="flex h-full w-full flex-col">
+      <div className="flex h-full w-full">
+        <div className="navSettings">
+          <NavContainer>
+            <div className="text-l text-ellipsis break-all p-3 font-semibold">{t('Settings')}</div>
+            <ul className="flex flex-1 flex-col gap-1 p-1">
+              {menu.map((item) => (
+                <li key={item.id}>
+                  <div
+                    className={cn(
+                      item.href === tab || item.hrefAlias === tab
+                        ? 'text-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring'
+                        : 'text-muted-foreground',
+                      'flex cursor-pointer flex-row items-center break-all rounded-md px-2 py-2 transition-colors duration-200 hover:text-accent-foreground',
+                    )}
+                    role="button"
+                    tabIndex={0}
+                    onKeyUp={(e) => {
+                      e.stopPropagation();
+                    }}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      onTabChanged(item.href as string);
+                    }}
+                  >
+                    <div className="relative flex-1 overflow-hidden text-ellipsis break-all">
+                      {t(item.name)}
+                    </div>
                   </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </NavContainer>
+                </li>
+              ))}
+            </ul>
+          </NavContainer>
+        </div>
+        <div className="contentSettings h-full flex-grow p-6  pt-16">
+          <div className="flex h-full flex-col pt-8">{children}</div>
+        </div>
       </div>
-      <div className="contentSettings h-full flex-grow p-6  pt-16">
-        <div className="flex h-full flex-col pt-8">{children}</div>
+      <div className="flex w-full gap-2 p-2 text-xs text-muted-foreground">
+        Opla version: {process.env.NEXT_PUBLIC_SENTRY_RELEASE} <Separator orientation="vertical" />
+        {process.env.NODE_ENV}
       </div>
     </div>
   );
