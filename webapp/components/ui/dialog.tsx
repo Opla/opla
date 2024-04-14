650fa352687dd6842870d438bea1d730ff7c57f3
Mik <mik@miklabs.com>
2024-04-14 21:07:27 +0200

    fix: create legacy dialog #728 (#783)

--- webapp/components/ui/dialog.tsx
+++ webapp/components/ui/dialog.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { LegacyRef, useCallback, useEffect, useRef } from 'react';
+import React, { LegacyRef, forwardRef, useCallback, useEffect, useRef } from 'react';
 import { X } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { Button } from './button';
@@ -22,6 +22,46 @@ function DialogClose({ className, onClose }: { className?: string; onClose: () =
 }
 DialogClose.displayName = 'DialogClose';
 
+const DialogOverlay = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  (
+    { className, ...props }: React.HTMLAttributes<HTMLDivElement>,
+    ref: LegacyRef<HTMLDivElement> | undefined,
+  ) => (
+    <div
+      ref={ref}
+      className={cn(
+        'fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+
+DialogOverlay.displayName = 'DialogOverlay';
+
+const LegacyDialog = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  (
+    { className, children, ...props }: React.HTMLAttributes<HTMLDivElement>,
+    ref: LegacyRef<HTMLDivElement> | undefined,
+  ) => (
+    <>
+      <DialogOverlay />
+      <div
+        ref={ref}
+        className={cn(
+          'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
+          className,
+        )}
+        {...props}
+      >
+        {children}
+      </div>
+    </>
+  ),
+);
+LegacyDialog.displayName = 'Dialog';
+
 function Dialog({
   id,
   title,
@@ -41,7 +81,8 @@ function Dialog({
   children: React.ReactNode;
 }) {
   const modalRef = useRef<HTMLDialogElement>(null);
-  // Dialog setOpen TODO
+  const legacyModalRef = useRef<HTMLDivElement>(null);
+  const nativeDialog = typeof HTMLDialogElement === 'function';
 
   const handleDialogClick = useCallback(
     (event: MouseEvent) => {
@@ -56,16 +97,27 @@ function Dialog({
 
   useEffect(() => {
     const dialog = modalRef;
-    dialog.current?.addEventListener('click', handleDialogClick);
-    if (open) {
-      dialog.current?.showModal();
-    } else {
-      dialog.current?.close();
+    const legacyDialog = legacyModalRef;
+    if (nativeDialog && dialog.current) {
+      dialog.current?.addEventListener('click', handleDialogClick);
+      if (open) {
+        dialog.current.showModal();
+      } else {
+        dialog.current.close();
+      }
+    } else if (!nativeDialog && legacyDialog.current) {
+      legacyDialog.current?.addEventListener('click', handleDialogClick);
+      if (open) {
+        legacyDialog.current.classList.remove('hidden');
+      } else {
+        legacyDialog.current.classList.add('hidden');
+      }
     }
     return () => {
       dialog.current?.removeEventListener('click', handleDialogClick);
+      legacyDialog.current?.removeEventListener('click', handleDialogClick);
     };
-  }, [handleDialogClick, onClose, open]);
+  }, [handleDialogClick, onClose, open, nativeDialog]);
 
   let cssSize;
   switch (size) {
@@ -86,6 +138,15 @@ function Dialog({
       break;
   }
 
+  if (!nativeDialog) {
+    return (
+      <LegacyDialog ref={legacyModalRef}>
+        {title && <div>{title}</div>}
+        {children}
+        <DialogClose onClose={onClose} />
+      </LegacyDialog>
+    );
+  }
   return (
     <dialog
       id={id}
@@ -102,44 +163,6 @@ function Dialog({
     </dialog>
   );
 }
-/* const DialogOverlay = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Overlay>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
->(({ className, ...props }, ref) => (
-  <DialogPrimitive.Overlay
-    ref={ref}
-    className={cn(
-      "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      className
-    )}
-    {...props}
-  />
-))
-DialogOverlay.displayName = DialogPrimitive.Overlay.displayName */
-
-/* const DialogContent = React.forwardRef<
-  React.ElementRef<typeof DialogPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => (
-  <DialogPortal>
-    <DialogOverlay />
-    <DialogPrimitive.Content
-      ref={ref}
-      className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
-        className
-      )}
-      {...props}
-    >
-      {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <Cross2Icon className="h-4 w-4"  strokeWidth={1.5} />
-        <span className="sr-only">Close</span>
-      </DialogPrimitive.Close>
-    </DialogPrimitive.Content>
-  </DialogPortal>
-))
-DialogContent.displayName = DialogPrimitive.Content.displayName */
 
 function DialogHeader({ className, children, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
