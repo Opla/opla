098c3137d85691bd242f34fb0e229874baacc57b
Mik <mik@miklabs.com>
2024-04-19 12:44:10 +0200

    chore: don't deploy if no changes in static #497 (#804)

---
+++ bin/check-docs.sh
@@ -0,0 +1,11 @@
+CHANGED=$(git diff --name-only bin)
+if [[ -z "${CHANGED}" ]]
+then
+      echo "No change."
+      echo $CHANGED
+      exit 0
+else
+      echo "Changed."
+      echo $CHANGED
+      exit 1
+fi
