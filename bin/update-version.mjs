0251bbb4386efdbbc94ce6cbb7efdae4657d8ba3
Mik <mik@miklabs.com>
2024-04-19 11:11:54 +0200

    chore: update all versions during Ga publish #494 (#803)

---
+++ bin/update-version.mjs
@@ -0,0 +1,29 @@
+import PackageJson from "@npmcli/package-json";
+import { writeFile } from 'fs/promises';
+
+const version = process.argv[2];
+const [semVer, major, minor, patch, prerelease, buildmetadata] = version.match(/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/) ?? [];
+let prereleaseName;
+let prereleaseVersion;
+if (prerelease?.indexOf('.') > 0) {
+    ([prereleaseName, prereleaseVersion = 0] = prerelease?.split('.') || []);
+} else {
+    prereleaseVersion = prerelease || '0';
+}
+const msiVersion = `${major}.${minor}.${Number(patch)*1000 + Number(prereleaseVersion)}`;
+console.log('semVer', {semVer, major, minor, patch, prerelease, prereleaseName, prereleaseVersion, buildmetadata, msiVersion});
+
+const pkgJson = await PackageJson.load("./webapp");
+console.log('previous Version=', pkgJson.content.version);
+pkgJson.update({
+    version
+});
+await pkgJson.save();
+
+const tauriWindowsToml =
+`# Dirty hack to fix Windows version not fully compatible with semver
+[package]
+version = "${msiVersion}"`;
+await writeFile('./webapp/native/Tauri.windows.toml', tauriWindowsToml);
+
+
