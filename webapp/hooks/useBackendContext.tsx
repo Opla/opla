f643771c43818f762c0cf41d63c7eed9d1234b7f
mikbry <mik@miklabs.com>
2024-07-23 19:10:11 +0200

    chore(deps): fix locks, eslint and remove rusqlite

--- webapp/hooks/useBackendContext.tsx
+++ webapp/hooks/useBackendContext.tsx
@@ -300,7 +300,7 @@ function BackendProvider({ children }: { children: React.ReactNode }) {
         }
       }
     },
-    [updateBackendStore, activeService, setActiveService],
+    [activeService, setActiveService],
   );
 
   const downloadListener = useCallback(
