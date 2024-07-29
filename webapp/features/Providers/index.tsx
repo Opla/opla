b565b00a0a2408afb0fa0d3a31158853063e40e1
mikbry <mik@miklabs.com>
2024-07-29 17:01:21 +0200

    fix: providers load storages

--- webapp/features/Providers/index.tsx
+++ webapp/features/Providers/index.tsx
@@ -12,11 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { useEffect, useRef } from 'react';
 import MainView from '@/components/common/MainView';
+import { useModelsStore, useProviderStore } from '@/stores';
+import { StorageState } from '@/stores/types';
 import Explorer from './Explorer';
 import ProviderView from './Provider';
 
+
+
+
 export default function Providers({ selectedProviderId }: { selectedProviderId?: string }) {
+  const { state, loadProviders } = useProviderStore();
+  const { loadModels } = useModelsStore();
+  const init = useRef<boolean>(true);
+  useEffect(() => {
+    if (init.current && state === StorageState.INIT) {
+      init.current = false;
+      loadProviders();
+      loadModels();
+    }
+  }, [state, loadModels, loadProviders]);
   return (
     <MainView selectedId={selectedProviderId} explorer={Explorer} contentView={ProviderView} />
   );
